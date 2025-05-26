e540b692abdc44b0c3f432835de4a8b1763fb330
Matheus <dev.matheusrm@gmail.com>
2025-05-26 14:28:17 -0300

    Correções na classe TransacaoRepository

--- src/main/java/com/itau/transacao_api/repository/TransacaoRepository.java
+++ src/main/java/com/itau/transacao_api/repository/TransacaoRepository.java
@@ -4,13 +4,22 @@ import com.itau.transacao_api.model.Transacao;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class TransacaoRepository {
-    private final List<Transacao> lista = Collections.synchronizedList(new ArrayList<>());
-    public void salvar(Transacao t) { lista.add(t); }
-    public void limpar() { lista.clear(); }
-    public List<Transacao> todas() { return new ArrayList<>(lista); }
+
+    private static final List<Transacao> transacoes = new ArrayList<>();
+
+    public void salvar(Transacao transacao) {
+        transacoes.add(transacao);
+    }
+
+    public List<Transacao> todas() {
+        return new ArrayList<>(transacoes);
+    }
+
+    public void limpar() {
+        transacoes.clear();
+    }
 }
